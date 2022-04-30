9d43e5601ffa642d3f312e1c161af922700954ad
Amrutha Narendran Chakkiyath <amrutha.narendran@st.ovgu.de>
2022-04-30 12:51:38 +0200

    Added bottom navigationbar files

---
+++ app/src/main/java/com/droidbits/moneycontrol/MainActivity.java
@@ -0,0 +1,53 @@
+package com.droidbits.moneycontrol;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.droidbits.moneycontrol.ui.categories.CategoriesFragment;
+import com.droidbits.moneycontrol.ui.home.HomeFragment;
+import com.droidbits.moneycontrol.ui.settings.SettingsFragment;
+import com.droidbits.moneycontrol.ui.transactions.TransactionFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+    }
+
+   private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+           new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.transactions:
+                            selectedFragment = new TransactionFragment();
+                            break;
+                        case R.id.categories:
+                            selectedFragment = new CategoriesFragment();
+                            break;
+                        case R.id.settings:
+                            selectedFragment = new SettingsFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    return true;
+       }
+   };
+}
