346ab26fbd918aeff3c6ecb1208d1c597262ead3
harig <hari.gokulam22@gmail.com>
2022-05-08 11:16:34 +0200

    Added bottom navigationbar files

--- app/src/main/java/com/droidbits/moneycontrol/ui/transactions/AddTransactionFragment.java
+++ app/src/main/java/com/droidbits/moneycontrol/ui/transactions/AddTransactionFragment.java
@@ -1,19 +1,87 @@
 package com.droidbits.moneycontrol.ui.transactions;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.droidbits.moneycontrol.R;
+import android.view.View;
+import android.widget.TextView;
 
-public class AddTransactionFragment extends Fragment {
+import java.util.Calendar;
+
+
+public class AddTransactionFragment extends Fragment{
+    EditText etDate;
+    DatePickerDialog.OnDateSetListener setListener;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_transactions, container,false);
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_add_transactions, container,false);
+        Spinner myspinner = v.findViewById((R.id.spinnerTransactionType));
+        ArrayAdapter myadapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.transaction_type));
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(myadapter);
+
+        Spinner myspinner2 = v.findViewById((R.id.spinnerTransactionMethod));
+        ArrayAdapter myadapter2 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.transaction_method));
+        myadapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner2.setAdapter(myadapter2);
+
+        etDate = v.findViewById(R.id.transactionDate);
+        Calendar calendar = Calendar.getInstance();
+        final int year = calendar.get(Calendar.YEAR);
+        final int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        etDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        getActivity(), new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        month = month+1;
+                        String date = day+"/"+month+"/"+year;
+                        etDate.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+
+        AppCompatButton addTransactionLayout = v.findViewById(R.id.button2);
+        addTransactionLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Fragment fragment = new TransactionFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return v;
+
     }
+
 }
