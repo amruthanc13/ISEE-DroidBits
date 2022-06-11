bad94f65c4315f58d10f8ee3165ffd651307d5c8
amrutha nc <amruthanarendranc@gmail.com>
2022-06-11 16:28:15 +0200

    adding FAQs with dummy questions and answers

--- app/src/main/java/com/droidbits/moneycontrol/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/droidbits/moneycontrol/ui/settings/SettingsFragment.java
@@ -4,9 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.droidbits.moneycontrol.R;
 
@@ -14,6 +17,21 @@ public class SettingsFragment extends Fragment{
         @Nullable
         @Override
         public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            return inflater.inflate(R.layout.fragment_settings, container,false);
+            View view = inflater.inflate(R.layout.fragment_settings, container,false);
+
+            Button FAQButton = view.findViewById(R.id.helpButton);
+            FAQButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(final View v)  {
+                    Fragment fragment = new HelpMenuFragment();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.fragment_container, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            });
+
+            return view;
         }
 }
