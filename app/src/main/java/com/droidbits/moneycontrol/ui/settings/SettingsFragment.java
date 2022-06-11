bc5a497fa66c6859e7942687a6940a0e08462a26
Amrutha Nc <amruthanarendranc@gmail.com>
2022-06-11 20:00:30 +0200

    adding end points to settings page

--- app/src/main/java/com/droidbits/moneycontrol/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/droidbits/moneycontrol/ui/settings/SettingsFragment.java
@@ -14,55 +14,73 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.droidbits.moneycontrol.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SettingsFragment extends Fragment{
-        @Nullable
-        @Override
-        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment_settings, container,false);
 
-            // onclick event for FAQ button
-            Button FAQButton = view.findViewById(R.id.helpButton);
-            FAQButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v)  {
-                    Fragment fragment = new HelpMenuFragment();
-                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.fragment_container, fragment);
-                    fragmentTransaction.addToBackStack(null);
-                    fragmentTransaction.commit();
-                }
-            });
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container,false);
 
-            // onclick event for log out button
-            Button logOutButton = view.findViewById(R.id.logout_button);
-            logOutButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(final View v) {
-                    MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
-                    builder.setTitle("Exit Application?");
-                    builder.setMessage("Are you sure you want to logout and exit from finzo?");
-                    builder.setBackground(getContext().getDrawable(
-                            (R.drawable.alert_dialogue_box)));
-                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(final DialogInterface dialog, final int which) {
-                            getActivity().finish();
-                            System.exit(0);
-                        }
-                    });
-                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                        @Override
-                        public void onClick(final DialogInterface dialog, final int which) {
-                            dialog.cancel();
-                        }
-                    });
-                    builder.show();
-                }
-            });
+        // onclick event for FAQ button
+        Button FAQButton = view.findViewById(R.id.helpButton);
+        FAQButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v)  {
+                Fragment fragment = new HelpMenuFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        // onclick event for set defaults button
+        Button DefaultSetButton = view.findViewById(R.id.default_button);
+        DefaultSetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v)  {
+                Fragment fragment = new DefaultValuesFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        // onclick event for log out button
+        Button logOutButton = view.findViewById(R.id.logout_button);
+        logOutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+                builder.setTitle("Exit Application?");
+                builder.setMessage("Are you sure you want to logout and exit from finzo?");
+                builder.setBackground(getContext().getDrawable(
+                        (R.drawable.alert_dialogue_box)));
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, final int which) {
+                        getActivity().finish();
+                        System.exit(0);
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(final DialogInterface dialog, final int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.show();
+            }
+        });
 
 
-            return view;
-        }
+        return view;
+    }
+
+
 }
