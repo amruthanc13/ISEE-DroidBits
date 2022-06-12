d17dbdd3212586808a4e8e46162671563349c661
Amrutha Nc <amruthanarendranc@gmail.com>
2022-06-12 16:07:22 +0200

    added card view end points for cumulative avg

---
+++ app/src/main/java/com/droidbits/moneycontrol/ui/home/FeaturedAdapter.java
@@ -0,0 +1,77 @@
+package com.droidbits.moneycontrol.ui.home;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import com.droidbits.moneycontrol.R;
+
+public class FeaturedAdapter extends RecyclerView.Adapter<FeaturedAdapter.FeaturedViewHolder>  {
+
+    private ArrayList<FeaturedHelperClass> featuredLocations;
+
+    /**
+     * Featured adapter.
+     * @param featuredLocationsFunction array of postions
+     */
+    public FeaturedAdapter(final ArrayList<FeaturedHelperClass> featuredLocationsFunction) {
+        this.featuredLocations = featuredLocationsFunction;
+    }
+
+    /**
+     * to assign the layout id.
+     */
+    @NonNull
+    @Override
+    public FeaturedViewHolder onCreateViewHolder(final @NonNull ViewGroup parent, final int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.featured_card_design, parent, false);
+        FeaturedViewHolder featuredViewHolder = new FeaturedViewHolder(view);
+        return featuredViewHolder;
+    }
+
+    /**
+     * get holder overrided function.
+     */
+    @Override
+    public void onBindViewHolder(final @NonNull FeaturedViewHolder holder, final int position) {
+
+        FeaturedHelperClass featuredHelperClass = featuredLocations.get(position);
+
+        holder.title.setText(featuredHelperClass.getTitle());
+        holder.expense.setText(featuredHelperClass.getExpense());
+        holder.income.setText(featuredHelperClass.getIncome());
+    }
+
+    /**
+     * Get item count.
+     * @return size
+     */
+    @Override
+    public int getItemCount() {
+        return featuredLocations.size();
+    }
+
+    public static class FeaturedViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView title, expense, income;
+
+        /**
+         * View holder.
+         * @param itemView view
+         */
+        public FeaturedViewHolder(final @NonNull View itemView) {
+            super(itemView);
+
+            title = itemView.findViewById(R.id.featured_text);
+            expense = itemView.findViewById(R.id.featured_expense_amount);
+            income = itemView.findViewById(R.id.featured_income_amount);
+        }
+    }
+}
---
+++ app/src/main/java/com/droidbits/moneycontrol/ui/home/FeaturedHelperClass.java
@@ -0,0 +1,36 @@
+package com.droidbits.moneycontrol.ui.home;
+
+public class FeaturedHelperClass {
+
+    private String title, expense, income;
+
+    public FeaturedHelperClass(String title, String expense, String income) {
+        this.title = title;
+        this.expense = expense;
+        this.income = income;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getExpense() {
+        return expense;
+    }
+
+    public void setExpense(String expense) {
+        this.expense = expense;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+}
